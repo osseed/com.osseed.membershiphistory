b68ff8c84f2f2bcdd8fed80d210ef26d2bc22c96
pranav45 <pranav.mainkar@osseed.com>
2020-12-09 17:40:38 +0530

    feat: fixed minor contact id issue.

--- CRM/Membershiphistory/Page/MembershipHistory.php
+++ CRM/Membershiphistory/Page/MembershipHistory.php
@@ -6,7 +6,7 @@ class CRM_Membershiphistory_Page_MembershipHistory extends CRM_Core_Page {
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('Membership History'));
-    $id = $_GET['cid'];
+    $id = isset($_GET['cid']) ? $_GET['cid'] : null;
     $membership_result = civicrm_api3('Membership', 'get', [
       'sequential' => 1,
       'contact_id' => $id,
